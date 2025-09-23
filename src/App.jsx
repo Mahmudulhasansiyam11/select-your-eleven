0a3c8984a1cb26ea3882040c089e7b8676edaaf8
Mahmudulhasansiyam11 <mahmudulsiyam766@gmail.com>
2025-09-23 01:38:02 -0700

    add state management and toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 
-import { Suspense } from 'react';
-import './App.css'
-import AvailablePlayers from './Components/AvailablePlayers/AvailablePlayers'
-import Navbar from './Components/Navbar/Navbar'
+import { Suspense, useState } from 'react';
+import './App.css';
+import AvailablePlayers from './Components/AvailablePlayers/AvailablePlayers';
+import Navbar from './Components/Navbar/Navbar';
+import SelectedPlayers from './Components/SelectedPlayers/SelectedPlayers';
 
 function App() {
+
+  const [toggle, setToggle] = useState(true);
   
   const fetchPlayers = async () => {
     const res = await fetch('/Players.json');
@@ -16,9 +19,19 @@ function App() {
   return (
     <>
     <Navbar></Navbar>
-    <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
+    <div className='mt-[30px] mb-[32px] max-w-[1200px] mx-auto flex justify-between items-center'>
+      <div className='font-bold text-[28px]'>Available Players</div>
+      <div>
+        <button onClick={() => setToggle(true)} className={`px-5 py-4 border-gray-200 border-[2px] border-r-0 font-bold rounded-l-2xl ${toggle === true ? "bg-[#E7FE29]" : " "}`}>Available</button>
+        <button onClick={() => setToggle(false)} className={`px-5 py-4 border-gray-200 border-[2px] border-l-0 font-bold rounded-r-2xl ${toggle === false ? "bg-[#E7FE29]" : " "}`}>Selected <span>(0)</span></button>
+      </div>
+    </div>
+
+    {
+      toggle === true ?  <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
       <AvailablePlayers fetchPlayersPromise={fetchPlayersPromise}></AvailablePlayers>
-    </Suspense>
+    </Suspense> : <SelectedPlayers></SelectedPlayers>
+    }
     </>
   )
 }
